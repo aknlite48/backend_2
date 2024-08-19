ada43a848ab1be8cdc967bcce181744f43264531
aknlite48 <aanandk2705@gmail.com>
2024-08-19 02:39:26 -0400

    added mongodb

---
+++ mongo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+if (process.argv.length<3) {
+  console.log("not sufficient args")
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://ak4634:${password}@cluster0.8lsje.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
